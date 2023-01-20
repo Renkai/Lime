7d9eb04e0d99be3fce46e1efe1d26ce872011879
Renkai Ge <renkaige@tubi.tv>
2023-01-20 19:17:49 +0800

    自然快手辅码短 手心格式

--- python/extract.py
+++ python/extract.py
@@ -1,7 +1,7 @@
 # http://bbs.pinyin.thunisoft.com/forum.php?mod=viewthread&tid=28002&highlight=%D7%D4%C8%BB%BF%EC%CA%D6
 
 def extract():
-    ubxn = open("自然快手手心.txt", "w")
+    ubxn = open("自然快手手心短.txt", "w")
     count = 0
     with open("蓝天快手单字.ini", "r", encoding="utf-16le") as f:
         lines = f.readlines()
@@ -14,11 +14,12 @@ def extract():
                 continue
 
             code = line[2:4].strip()
+            short = line[2:3].strip()
             char = line[6:].strip()
             print(f"line: {line}")
             print(char)
             print(code)
-            ubxn.write(f"{char}={code}\n")
+            ubxn.write(f"{char}={short}\n")
 
 
 if __name__ == '__main__':
